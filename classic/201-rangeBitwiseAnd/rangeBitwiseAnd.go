60cabcb504b0e884d12d5e1848e956059bd60110
minsky <mingorun@163.com>
2025-07-22 14:49:41 +0800

    增加201 数字范围按位与

---
+++ classic/201-rangeBitwiseAnd/rangeBitwiseAnd.go
@@ -0,0 +1,25 @@
+package _01_rangeBitwiseAnd
+
+// 给你两个整数 left 和 right ，表示区间 [left, right] ，返回此区间内所有数字 按位与 的结果（包含 left 、right 端点）。
+//
+// 示例 1：
+//
+// 输入：left = 5, right = 7
+// 输出：4
+// 示例 2：
+//
+// 输入：left = 0, right = 0
+// 输出：0
+// 示例 3：
+//
+// 输入：left = 1, right = 2147483647
+// 输出：0
+//
+//
+// 提示：
+//
+// 0 <= left <= right <= 231 - 1
+
+func rangeBitwiseAnd(left int, right int) int {
+
+}
